8d95bfb917bfcb26f3755953c928688b43a3ee54
Garrett Rowe <garrett.m.rowe@gmail.com>
2022-03-20 10:20:21 -0700

    Add scroll to first error in compilation window

--- init.el
+++ init.el
@@ -156,7 +156,8 @@
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
  '(package-selected-packages
-   '(org-superstar company ## which-key speed-type smex undo-tree ivy paredit projectile cider use-package)))
+   '(org-superstar company ## which-key speed-type smex undo-tree ivy paredit projectile cider use-package))
+ '(safe-local-variable-values '((compilation-scroll-output . t))))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
